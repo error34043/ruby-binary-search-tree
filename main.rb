218f8f705e531c0000fb7ce063768ace3f3ff629
Sanjana <sanjhemdev@gmail.com>
2020-08-28 18:00:55 +0530

    Build Node class method to compare data attributes

--- main.rb
+++ main.rb
@@ -1,2 +1,17 @@
 # frozen-string-literal: true
 
+# Represents a single node
+class Node
+  include Comparable
+  attr_accessor :data, :left_child, :right_child
+
+  def <=>(other)
+    @data <=> other.data
+  end
+
+  def initialize(data, left_child = nil, right_child = nil)
+    @data = data
+    @left_child = left_child
+    @right_child = right_child
+  end
+end
